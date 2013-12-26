ebb8beb873f95435bb4f237ceead507679e03fbe
Sawyer Pangborn <spangborn@users.noreply.github.com>
2013-12-26 10:55:41 -0700

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,7 +2,7 @@
 echo 'Installing Pebble SDK Dependencies...'
 
 # Get the Pebble SDK and toolchain
-wget https://developer.getpebble.com/2/download/$(PEBBLESDK).tar.gz PebbleSDK.tar.gz
+wget https://developer.getpebble.com/2/download/$(PEBBLE_SDK).tar.gz PebbleSDK.tar.gz
 wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
 # Build the Pebble directory
@@ -12,12 +12,12 @@ cd pebble-dev
 
 # Extract the SDK
 tar -zxf ../PebbleSDK.tar.gz
-cd $(PEBBLESDK)
+cd $(PEBBLE_SDK)
 
 # Extract the toolchain
 tar -zxf ../../arm-cs-tools-ubuntu-universal.tar.gz
 
-export PATH=~/pebble-dev/PebbleSDK-$(PEBBLESDK)/bin:$PATH
+export PATH=~/pebble-dev/PebbleSDK-$(PEBBLE_SDK)/bin:$PATH
 
 
 
