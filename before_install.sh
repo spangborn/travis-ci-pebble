5a81f9dd1b52ef15a55902d974f73c6c566c1ec1
Alexander Kropochev <alexander@MacBook-Pro.local>
2015-03-26 21:33:44 +0500

    Fix Pebble SDK url

--- before_install.sh
+++ before_install.sh
@@ -4,7 +4,7 @@ echo 'Installing Pebble SDK Dependencies...'
 cd ~ 
 
 # Get the Pebble SDK and toolchain
-wget https://developer.getpebble.com/2/download/${PEBBLE_SDK}.tar.gz -O PebbleSDK.tar.gz
+wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/${PEBBLE_SDK}.tar.gz -O PebbleSDK.tar.gz
 wget http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
 
 # Build the Pebble directory
